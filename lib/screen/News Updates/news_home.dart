6a0f36c661891a0297a24bef6b77e6b1d4864325
Rijan Rayamajhi <96490513+xrijan@users.noreply.github.com>
2023-04-09 04:35:49 +0530

    #updated

--- lib/screen/News Updates/news_home.dart
+++ lib/screen/News Updates/news_home.dart
@@ -1,10 +1,12 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/colors.dart';
 import 'package:news_app/helper/news_data.dart';
+import '../../helper/news_category_data.dart';
+import '../../model/news_category_model.dart';
 import '../../model/news_model.dart';
-import '../../widgets/bottom_nav_bar.dart';
 import '../../widgets/news_of_day.dart';
+import '../NewsCetegory/news_category.dart';
+
 
 class NewsHome extends StatefulWidget {
   const NewsHome({Key? key}) : super(key: key);
@@ -19,6 +21,7 @@ class _NewsHomeState extends State<NewsHome> {
 
   //get the list of the news
   List<NewsModel> newsArticle = [];
+  List<NewsCategoryModel> newsCategory = [];
 
   getNews() async {
     NewsData newsData = NewsData();
@@ -33,165 +36,199 @@ class _NewsHomeState extends State<NewsHome> {
     // TODO: implement initState
     super.initState();
     getNews();
+    newsCategory = getCategory();
   }
 
   bool hasData = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: Padding(
-          padding: const EdgeInsets.only(left: 16),
-          child: Container(
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              border: Border.all(
-                color: Colors.black,
-                width: 2,
-              ),
-            ),
-            child: const CircleAvatar(
-              backgroundImage: NetworkImage(
-                'https://expertphotography.b-cdn.net/wp-content/uploads/2018/10/cool-profile-pictures-retouching-1.jpg',
-              ),
-              backgroundColor: Colors.grey,
-              radius: 20, // change the radius to 20
-            ),
-          ),
-        ),
-        actions: [
-          Container(
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: CustomColor.secondaryColor,
-              border: Border.all(
-                color: Colors.grey.shade50,
-                width: 2,
-              ),
-            ),
-            child: IconButton(
-              onPressed: () {
-                // handle search button press
-              },
-              icon: const Icon(Icons.search),
-              color: Colors.black,
-            ),
-          ),
-          const SizedBox(width: 16),
-          Padding(
-            padding: const EdgeInsets.only(right: 16),
-            child: Container(
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: CustomColor.secondaryColor,
-                border: Border.all(
-                  color: Colors.grey.shade50,
-                  width: 2,
-                ),
-              ),
-              child: IconButton(
-                onPressed: () {
-                  // handle liked button press
-                },
-                icon:Stack(
-                  children: [
-                    const Icon(Icons.favorite_border),
-                    if (hasData)
-                      Positioned(
-                        top: 0,
-                        right: 0,
-                        child: Container(
-                          width: 8,
-                          height: 8,
-                          decoration: const BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.red,
-                          ),
-                        ),
-                      ),
-                  ],
-                ),
-                color: Colors.black,
-              ),
-            ),
-          ),
-        ],
-
-      ),
       backgroundColor: CustomColor.primaryColor,
-      bottomNavigationBar: const CustomNavBar(index: 0),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          SizedBox(height: 20,),
-          const NewsOfTheDay(),
-          Padding(
-            padding: const EdgeInsets.only(left: 25, top: 20, right: 25),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  "Today Highlights",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AppBar(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              leading: Padding(
+                padding: const EdgeInsets.only(left: 16),
+                child: SingleChildScrollView(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 2,
+                      ),
+                    ),
+                    child: const CircleAvatar(
+                      backgroundImage: NetworkImage(
+                        'https://expertphotography.b-cdn.net/wp-content/uploads/2018/10/cool-profile-pictures-retouching-1.jpg',
+                      ),
+                      backgroundColor: Colors.grey,
+                      radius: 20, // change the radius to 20
+                    ),
+                  ),
                 ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/highlights');
-                  },
-                  child: const Text('Show More'),
-                )
+              ),
+              actions: [
+                Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: CustomColor.secondaryColor,
+                    border: Border.all(
+                      color: Colors.grey.shade50,
+                      width: 2,
+                    ),
+                  ),
+                  child: IconButton(
+                    onPressed: () {
+                      // handle search button press
+                    },
+                    icon: const Icon(Icons.search),
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Padding(
+                  padding: const EdgeInsets.only(right: 16),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: CustomColor.secondaryColor,
+                      border: Border.all(
+                        color: Colors.grey.shade50,
+                        width: 2,
+                      ),
+                    ),
+                    child: IconButton(
+                      onPressed: () {
+                        // handle liked button press
+                      },
+                      icon: Stack(
+                        children: [
+                          const Icon(Icons.bookmark_border),
+                          if (hasData)
+                            Positioned(
+                              top: 0,
+                              right: 0,
+                              child: Container(
+                                width: 8,
+                                height: 8,
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.red,
+                                ),
+                              ),
+                            ),
+                        ],
+                      ),
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
               ],
             ),
-          ),
-          Expanded(
-            child: ListView.builder(
-               padding: EdgeInsets.zero,
-                itemCount: newsArticle.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Row(
+            const SizedBox(
+              height: 20,
+            ),
+            const NewsOfTheDay(),
+            const Padding(
+              padding: EdgeInsets.all(25),
+              child: Text(
+                "Explore",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const NewsCategoryScreen(),
+            Padding(
+              padding: const EdgeInsets.only(left: 25, right: 25),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Today Highlights",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/highlights');
+                    },
+                    child: const Text(
+                      'Show More',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Column(
+              children: List.generate(newsArticle.length, (index) {
+                return Padding(
+                  padding: const EdgeInsets.only(left: 25, right: 25),
+                  child: Column(
                     children: [
-                      Container(
-                        height: 120,
-                        width: 120,
-                        decoration: BoxDecoration(
-                          color: Colors.grey,
-                          borderRadius: BorderRadius.circular(20),
-                          image: DecorationImage(
-                            image: NetworkImage(newsArticle[index].urlToImage),
-                            fit: BoxFit.cover,
+                      Row(
+                        children: [
+                          Container(
+                            height: 100,
+                            width: 80,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(
+                                  8), // Change the border radius value
+                              color: CustomColor.secondaryColor,
+                              image: DecorationImage(
+                                image: NetworkImage(imageUrl),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
                           ),
-                        ),
-                      ),
-                      const SizedBox(width: 10),
-                      Expanded(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              children:  [
-                                const Icon(Icons.person),
-                                Text(
-                                  newsArticle[index].author.length.toString(),
-                                  style: const TextStyle(color: Colors.grey),
+                          const SizedBox(width: 16),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text('News Category',
+                                    style: TextStyle(
+                                        fontSize: 16, color: Colors.grey)),
+                                Text(newsArticle[index].title,
+                                    style: const TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold)),
+                                const SizedBox(height: 4),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(newsArticle[index].publishedAt,
+                                        style: const TextStyle(
+                                            fontSize: 12, color: Colors.grey)),
+                                    IconButton(
+                                      icon: const Icon(Icons.bookmark_border),
+                                      onPressed: () {},
+                                    ),
+                                  ],
                                 ),
                               ],
                             ),
-                            Text(newsArticle[index].title),
-                             Text(
-                              newsArticle[index].publishedAt,
-                              style: const TextStyle(color: Colors.grey),
-                            )
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
+                      const Divider(
+                        color: Colors.grey,
+                      )
                     ],
-                  );
-                }),
-          ),
-        ],
+                  ),
+                );
+              }),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/screen/NewsCetegory/news_category.dart
+++ lib/screen/NewsCetegory/news_category.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/colors.dart';
 import 'package:news_app/helper/news_category_data.dart';
 import 'package:news_app/screen/NewsCetegory/news_catogeries_selected.dart';
 import '../../model/news_category_model.dart';
@@ -13,8 +12,6 @@ class NewsCategoryScreen extends StatefulWidget {
 }
 
 class _NewsCategoryScreenState extends State<NewsCategoryScreen> {
-  final String imageUrl =
-      'https://www.thesocialhistorian.com/wp-content/uploads/2016/01/Duke_Humfreys_Library_Interior_6_Bodleian_Library_Oxford_UK_-_Diliff-2048x1523.jpg';
   List<NewsCategoryModel> newsCategory = [];
   List<NewsModel> newsArticle = [];
 
@@ -30,139 +27,37 @@ class _NewsCategoryScreenState extends State<NewsCategoryScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: CustomColor.primaryColor,
-      appBar: AppBar(
-        backgroundColor: CustomColor.primaryColor,
-        title: const Text(
-          'Categories',
-          style: TextStyle(color: Colors.black),
-        ),
-        elevation: 0,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 20, right: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Breaking News',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Stack(
-              children: [
-                Container(
-                  height: 200,
-                  width: 450,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: Colors.grey,
-                  ),
-                  child: ClipRRect(
-                      borderRadius: BorderRadius.circular(8),
-                      child: Image.network(
-                        imageUrl,
-                        fit: BoxFit.fill,
-                      )),
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: SizedBox(
+        height: 100,
+        child: ListView.builder(
+          physics: const BouncingScrollPhysics(),
+          scrollDirection: Axis.horizontal,
+          itemCount: newsCategory.length,
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SelectedNewsCategories(title: newsCategory[index].name, category: newsCategory[index].name.toLowerCase(),)),
+                );
+              },
+              child: Container(
+                width: 100.0,
+                height: 100.0,
+                margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(50.0),
                 ),
-                Positioned.fill(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          Colors.transparent,
-                          Colors.black.withOpacity(0.8),
-                        ],
-                      ),
-                    ),
-                  ),
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(newsCategory[index].imageUrl),
+                  radius: 45.0,
                 ),
-                Positioned(
-                  bottom: 0,
-                  left: 0,
-                  right: 0,
-                  child: Container(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text(
-                          "Rao IIT Academy institute director held for 'Rs 14 crore GST fraud fggfghfghfgfh fgjffjfh ghhjghj'",
-                          style: TextStyle(color: Colors.white, fontSize: 14),
-                        ),
-                        const SizedBox(
-                          width: 5,
-                        ),
-                        TextButton(
-                          onPressed: () {},
-                          style: TextButton.styleFrom(padding: EdgeInsets.zero),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: const [
-                              Text("Learn More"),
-                              SizedBox(
-                                width: 10,
-                              ),
-                              Icon(Icons.near_me)
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Text(
-              'Generals',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Expanded(
-              child: GridView.builder(
-                itemCount: newsCategory.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  mainAxisSpacing: 10,
-                  crossAxisSpacing: 10,
-                ),
-                itemBuilder: (BuildContext context, int index) {
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => SelectedNewsCategories(title: newsCategory[index].name, category: newsCategory[index].name.toLowerCase(),)),
-                      );
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(16),
-                        color: Colors.grey,
-                      ),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(16),
-                        child: Image.network(
-                          newsCategory[index].imageUrl,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                  );
-                },
               ),
-            ),
-          ],
+            );
+          },
         ),
       ),
     );
